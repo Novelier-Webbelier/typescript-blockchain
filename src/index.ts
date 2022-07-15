b518820eb98b9fea169b221ceea8b0037e405703
Novelier-Webbelier <novelier.webbelier@gmail.com>
2022-07-15 22:42:22 +0900

    5.2 Lib Configuration

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-class Block {
-  constructor(private data: string) { }
-  static hello() {
-    return "Hi";
-  }
-}
+// class Block {
+//   constructor(private data: string) { }
+//   static hello() {
+//     return "Hi";
+//   }
+// }
+
+// Here is autocompelete when I add "DOM" to "lib" key into tsconfig.json file in this type-chain project.
+document.createElement("span");
+localStorage.getItem("Hello World");
